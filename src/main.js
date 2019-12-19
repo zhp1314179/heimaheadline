3bde861eab02acfed9a2d8f1c7323cd2df161729
baba <201694638@qq.com>
2019-12-19 09:15:14 +0800

    配置接口默认属性axios.defaults。baseurl

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import ElementUi from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css' // 引入elementui样式
 import './styles/index.less' // 引入初始化样式
+import axios from 'axios'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.use(ElementUi)
 new Vue({ // 全局注册
   router,
