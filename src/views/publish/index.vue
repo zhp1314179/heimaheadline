07ec45cd56a76ddd56d904dd9cefda2c4a0454ba
baba <201694638@qq.com>
2019-12-25 15:15:17 +0800

    文章内容前端页面

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,39 +3,57 @@
     <bread-crumb slot="header">
       <template slot="title">发布文章</template>
     </bread-crumb>
-    <el-form style="margin-left:50px">
-      <el-form-item label="标题">
-        <el-input style="width:60%"></el-input>
+    <el-form ref="publishForm" style="margin-left:50px" :model="formData" :rules="publishRules">
+      <el-form-item label="标题" prop="title">
+        <el-input v-model="formData.title" style="width:60%"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input style="width:92%" type="textarea" :rows="4"></el-input>
+      <el-form-item label="内容" prop="content">
+        <el-input v-model="formData.content" style="width:92%" type="textarea" :rows="4"></el-input>
       </el-form-item>
-      <el-form-item label="封面">
-        <el-radio-group>
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+      <el-form-item label="封面" prop="type">
+        <el-radio-group v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道">
-        <el-select placeholder="请选择">
+      <el-form-item prop="channel_id" label="频道">
+        <el-select placeholder="请选择" v-model="formData.channel_id">
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发布</el-button>
-        <el-button>存入草稿</el-button>
+        <el-button type="primary" @click="publishArticle">发布</el-button>
+        <el-button @click="publishArticle">存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
 </template>
 
 <script>
+
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        cover: {
+          type: 0,
+          images: []
+        },
+        channel_id: null
+      },
+      publishRules: {
+        title: [
+          { required: true, message: '标题内容不能为空' },
+          { min: 5, max: 30, message: '标题长度需要5-30个字符' }
+        ],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道内容不能为空' }]
+      }
     }
   },
   methods: {
@@ -46,6 +64,13 @@ export default {
         console.log(result)
 
         this.channels = result.data.channels
+      })
+    },
+    publishArticle () {
+      this.$refs.publishForm.validate(function (isOk) {
+        if (isOk) {
+          console.log('校验成功')
+        }
       })
     }
   },
